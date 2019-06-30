128eb889e73fb2cda3e5529c0140941e2ad041dc
oneNutW0nder <scb5436@rit.edu>
2019-06-29 22:02:08 -0400

    Added comments to the insanity

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"syscall"
 )
 
+// Function to do this err checking repeatedly
 func checkEr(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -13,27 +14,41 @@ func checkEr(err error) {
 }
 
 // htons converts a short (uint16) from host-to-network byte order.
+// #Stackoverflow
 func htons(i uint16) uint16 {
 	return (i<<8)&0xff00 | i>>8
 }
 
+// Reads the packet in from a socket file descriptor (fd)
+// fd int --> file descriptor that relates to the socket created in main
 func readPacket(fd int) {
+
 	// Buffer for packet data that is read in
 	buf := make([]byte, 1500)
-	// Get *Interface struct for the interface that we are using
 
-	read, sockaddr, err := syscall.Recvfrom(fd, buf, 0)
+	// Read in the packets
+	// num 		--> number of bytes
+	// sockaddr --> the sockaddr struct that the packet was read from
+	// err 		--> was there an error?
+	num, sockaddr, err := syscall.Recvfrom(fd, buf, 0)
 	checkEr(err)
-	fmt.Println(read, "bytes from", sockaddr)
+
+	// Debug shenanigans
+	fmt.Println(num, "bytes from", sockaddr)
 	fmt.Println(buf)
 }
 
 func main() {
 
+	// Create the socket
+	// AF_PACKET 	--> Low level packet interface access
+	// SOCK_RAW		--> Socket type is RAW
+	// ETH_P_ALL	--> Handle all Ethernet frames that come
 	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(syscall.ETH_P_ALL)))
 	checkEr(err)
 
 	for {
+		// REEEEEEEEEEEEEEEEEEEEEEEEEE
 		readPacket(fd)
 	}
 
