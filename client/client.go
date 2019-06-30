e73784159f2c420174accedb5d71a87da9288cf1
oneNutW0nder <scb5436@rit.edu>
2019-06-29 22:14:28 -0400

    Documentation

--- client/client.go
+++ client/client.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/gopacket/layers"
 )
 
+// Function to do this err checking repeatedly
 func checkEr(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -17,11 +18,19 @@ func checkEr(err error) {
 }
 
 // htons converts a short (uint16) from host-to-network byte order.
+// #Stackoverflow
 func htons(i uint16) uint16 {
 	return (i<<8)&0xff00 | i>>8
 }
 
+// This function takes a net.Interface pointer to access
+// 	things like the MAC Address... and yeah... the MAC Address
+//
+// ifaceInfo	--> pointer to a net.Interface
+//
+// Returns		--> Byte array that is a properly formed/serialized packet
 func createPacket(ifaceInfo *net.Interface) []byte {
+
 	// Create a new seriablized buffer
 	buf := gopacket.NewSerializeBuffer()
 
@@ -48,54 +57,77 @@ func createPacket(ifaceInfo *net.Interface) []byte {
 			Flags:      0x40,
 			FragOffset: 0,
 			Checksum:   0,
-			Protocol:   syscall.IPPROTO_UDP,
+			Protocol:   syscall.IPPROTO_UDP, // Sending a UDP Packet
 			DstIP:      net.IPv4(192, 168, 1, 57),
 			SrcIP:      net.IPv4(192, 168, 1, 57),
 		},
 		// UDP layer
 		&layers.UDP{
 			SrcPort:  6969,
-			DstPort:  layers.UDPPort(1337),
+			DstPort:  layers.UDPPort(1337), // Saw this used in some code @github... seems legit
 			Length:   26,
 			Checksum: 0, // TODO
 		},
 		// Set the payload
 		gopacket.Payload("Encapsulation work"),
 	)
+
 	// Save the newly formed packet and return it
 	packetData := buf.Bytes()
 
 	return packetData
 }
 
+// This function sends a packet using a provided
+//	socket file descriptor (fd)
+//
+// fd 		--> The file descriptor for the socket to use
+//
+// Returns 	--> None
 func sendPacket(fd int) {
-	// Get *Interface struct for the interface that we are using
-	info, _ := net.InterfaceByName("wlp4s0")
 
+	// Get *Interface struct for the interface that we are using
+	ifaceInfo, _ := net.InterfaceByName("wlp4s0")
+
+	// Create a byte array for the MAC Addr
 	var haddr [8]byte
-	copy(haddr[0:7], info.HardwareAddr[0:7])
+
+	// Copy the MAC from the interface struct in the new array
+	copy(haddr[0:7], ifaceInfo.HardwareAddr[0:7])
+
+	// Initialize the Sockaddr struct
 	addr := syscall.SockaddrLinklayer{
 		Protocol: syscall.ETH_P_IP,
-		Ifindex:  info.Index,
-		Halen:    uint8(len(info.HardwareAddr)),
+		Ifindex:  ifaceInfo.Index,
+		Halen:    uint8(len(ifaceInfo.HardwareAddr)),
 		Addr:     haddr,
 	}
 
+	// Bind the socket
 	checkEr(syscall.Bind(fd, &addr))
+
+	// Set promiscuous mode = true
 	checkEr(syscall.SetLsfPromisc("wlp4s0", true))
-	n, err := syscall.Write(fd, createPacket(info))
+
+	// Send a packet using our socket
+	// n --> number of bytes sent
+	n, err := syscall.Write(fd, createPacket(ifaceInfo))
 	checkEr(err)
 
+	// Debug shenanigans
 	fmt.Printf("Number of bytes written: %d", n)
-
-	//fmt.Println(info)
-	//fmt.Println(info.HardwareAddr)
 }
 
 func main() {
 
+	// Create the socket
+	// AF_PACKET 	--> Low level packet interface access
+	// SOCK_RAW		--> Socket type is RAWe
+	// ETH_P_ALL	--> Handle all Ethernet frames that come
 	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(syscall.ETH_P_ALL)))
 	checkEr(err)
+
+	// REEEEEEEEEEEEEEEEEEEEE
 	for {
 		sendPacket(fd)
 	}
