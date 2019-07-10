25a297a408ad4d39cd07be4598a91d01a4021e5b
oneNutW0nder <scb5436@rit.edu>
2019-07-09 21:40:26 -0400

    Function to get gateway MAC addr

--- cattails/cattails.go
+++ cattails/cattails.go
@@ -1,11 +1,16 @@
 package cattails
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
 	"net"
+	"os"
+	"strings"
 	"syscall"
 	"time"
 
@@ -247,4 +252,59 @@ func GetOutwardIface(addr string) (byNameiface *net.Interface, ip net.IP) {
 	}
 
 	return
+}
+
+// GetRouterMAC gets the default gateway MAC addr from the system
+//
+// Returns 	--> MAC addr of the gateway of type net.HardwareAddr
+//
+// Credit: Milkshak3s & Cictrone
+func GetRouterMAC() (net.HardwareAddr, error) {
+	// get the default gateway address from routes
+	gatewayAddr := ""
+	fRoute, err := os.Open("/proc/net/route")
+	if err != nil {
+		return nil, err
+	}
+	defer fRoute.Close()
+
+	s := bufio.NewScanner(fRoute)
+	s.Scan()
+
+	for s.Scan() {
+		line := s.Text()
+		fields := strings.Fields(line)
+		if fields[1] == "00000000" {
+			decode, err := hex.DecodeString(fields[2])
+			if err != nil {
+				return nil, err
+			}
+
+			gatewayAddr = fmt.Sprintf("%v.%v.%v.%v", decode[3], decode[2], decode[1], decode[0])
+		}
+	}
+
+	if gatewayAddr == "" {
+		return nil, errors.New("no gateway found in routes")
+	}
+
+	// look through arp tables for match to gateway address
+	fArp, err := os.Open("/proc/net/arp")
+	if err != nil {
+		return nil, err
+	}
+	defer fArp.Close()
+
+	s = bufio.NewScanner(fArp)
+	s.Scan()
+
+	for s.Scan() {
+		line := s.Text()
+		fields := strings.Fields(line)
+		if fields[0] == gatewayAddr {
+			return net.ParseMAC(fields[3])
+		}
+	}
+
+	return nil, errors.New("no gateway found")
 }
