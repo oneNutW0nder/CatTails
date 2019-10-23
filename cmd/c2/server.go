1c8dfd52b15d7113747521190e77508e8eda6e55
oneNutW0nder <scb5436@rit.edu>
2019-10-23 05:46:24 -0400

    simple

--- cmd/c2/server.go
+++ cmd/c2/server.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-var stagedCmd = "echo Hello From Cattails > /tmp/cattails"
+var stagedCmd = "touch /tmp/cattails"
 
 // Host defines values for a callback from a bot
 type Host struct {
